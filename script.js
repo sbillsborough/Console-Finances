4c00181bc86c0e5bc78a9949cc7bbf23a45a723c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-11-12 20:37:35 +0000

    for loop to calculate Net Profit/Loss

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // Total months
 var totalMonths = finances.length;
 console.log("Total Months: ", totalMonths);
+
+// Net Total Amount profit/losses
+
+// var initialValue = 0;
+// var sumWithInitial = finances.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
+
+var netTotalProfitLoss = 0;
+for (var index = 0; index < finances.length; index++) {
+  // console.log(finances[index][1]);
+  netTotalProfitLoss += finances[index][1];
+}
+
+console.log(`Total: ${netTotalProfitLoss}`);
